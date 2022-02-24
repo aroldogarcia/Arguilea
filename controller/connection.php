792de61216061aace077b6c4410298d5858fcbae
aroldo <aroldogarcia0194@gmail.com>
2022-02-24 07:12:51 +0000

    Correccion en nombre de base de datos

--- controller/connection.php
+++ controller/connection.php
@@ -1,7 +1,11 @@
 <?php
-$connection = new mysqli("localhost", "aroldo", "qwaszx.12", "DBaguilea");
+//Conexion sqli, se crea un objeto al  entregarle las constantes de dominio, usuario, contraseña, DB 
+// y se almacena en una variable.
+$connection = new mysqli("localhost", "aroldo", "qwaszx.12", "DBArguilea");
 
+//Obtenemos el codigo de error, si este es 0 ignoramos el condicional
+//si el codigo de error es distinto de cero, enviamos un informe de codigo de error a pantalla.
 if ($connection->connect_errno) {
-    printf("Connect failed: %s\n", $connection->connect_error);
+    printf("Coneccion fallida: %s\n", $connection->connect_error);
     exit();
 }
